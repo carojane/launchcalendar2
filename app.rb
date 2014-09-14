ee6effd21474bbd291cfd628db82aa74679c2e79
carojane <carojane@gmail.com>
2014-09-14 09:37:07 -0400

    renamed subdir 'days' as 'forms', added functionality to assignment form

--- app.rb
+++ app.rb
@@ -10,6 +10,10 @@ Dir[File.join(File.dirname(__FILE__), 'app', '**', '*.rb')].each do |file|
   require file
 end
 
+##########################################################
+##########################################################
+##########################################################
+
 def validate_url
 end
 
@@ -35,14 +39,39 @@ def add_focus(params)
   end
 end
 
+def add_assignment(params)
+  week = Week.find_by(campaign_id: params[:campaign].to_i, week_number: params[:week].to_i)
+  date = (week.week_start + params[:day].to_i)
+
+  if Assignment.find_by(name: params[:name]) == nil
+    assignment = Assignment.new(name: params[:name], campaign_id: params[:campaign], date: date)
+  else
+    assignment = Assignment.find_by(name: params[:name])
+    assignment.update(campaign_id: params[:campaign], date: date)
+  end
+
+  assignment.update(url: params[:url]) if params[:url] != ""
+
+  if params[:day] == "5"
+    assignment.update(extra: true)
+  else
+    assignment.update(extra: false)
+  end
+  binding.pry
+end
+
+##########################################################
+##########################################################
+##########################################################
+
 get '/' do
   @campaigns = Campaign.all.order(name: :asc)
 
   erb :index
 end
 
-get '/day' do
-  erb :'/day/index'
+get '/forms' do
+  erb :'/forms/index'
 end
 
 post '/day' do
@@ -53,11 +82,16 @@ post '/day' do
     redirect '/'
   end
 
-  erb :'/day/index'
+  erb :'/forms/index'
 end
 
 post '/assignment' do
-  erb :'/day/index'
+  if params[:week] && params[:name] != ""
+    add_assignment(params)
+    redirect '/'
+  end
+
+  erb :'/forms/index'
 end
 
 post '/week' do
@@ -66,5 +100,5 @@ post '/week' do
     redirect '/'
   end
 
-  erb :'/day/index'
+  erb :'/forms/index'
 end
